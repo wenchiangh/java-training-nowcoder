63cb66e03c586e217626bd94819a22e2c0b0bd4f
wenchianghsieh <98464039+wenchianghsieh@users.noreply.github.com>
2022-03-10 11:09:51 +0800

    bm3

---
+++ src/bm3/Solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+
+public class ListNode {
+    int val;
+    ListNode next = null;
+}
+
+public class Solution {
+    /**
+     * 
+     * @param head ListNode类 
+     * @param k int整型 
+     * @return ListNode类
+     */
+    public ListNode reverseKGroup (ListNode head, int k) {
+        // 创建头
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        
+    }
+}
