33a7d3f632f70ca07f746438cb382c2417b34a60
wch.acct@outlook.com <wch.acct@outlook.com>
2022-03-10 23:39:03 +0800

    BM16 删除有序链表中重复的元素-II

--- src/bm16/Solution.java
+++ src/bm16/Solution.java
@@ -10,5 +10,36 @@ class ListNode {
     }
 }
 
+/**
+ * BM16 删除有序链表中重复的元素-II
+ */
 public class Solution {
+    /**
+     * 
+     * @param head ListNode类
+     * @return ListNode类
+     */
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode pre = dummy;
+        ListNode curr = pre.next;
+
+        while (curr != null && curr.next != null) {
+            if (curr.val != curr.next.val) {
+                pre = curr;
+                curr = curr.next;
+            } else {
+                ListNode t = pre.next;
+                while (t != null && t.val == curr.val) {
+                    t = t.next;
+                }
+                pre.next = t;
+                curr = pre.next;
+            }
+        }
+
+        return dummy.next;
+    }
 }
