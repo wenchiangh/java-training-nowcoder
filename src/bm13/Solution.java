a24b11603849082ca623c2e3526d9c84950d9dac
wch.acct@outlook.com <wch.acct@outlook.com>
2022-03-10 22:39:50 +0800

    BM13 判断一个链表是否为回文结构

--- src/bm13/Solution.java
+++ src/bm13/Solution.java
@@ -10,5 +10,47 @@ class ListNode {
     }
 }
 
+/*
+ * BM13 判断一个链表是否为回文结构
+ */
 public class Solution {
+    /**
+     * 
+     * @param head ListNode类 the head
+     * @return bool布尔型
+     */
+    public boolean isPail(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if (fast != null) {
+            slow = slow.next;
+        }
+
+        ListNode dummy = new ListNode(-1);
+        while (slow != null) {
+            ListNode next = slow.next;
+            slow.next = dummy.next;
+            dummy.next = slow;
+            slow = next;
+        }
+
+        slow = dummy.next;
+        fast = head;
+
+        while (slow != null) {
+            if (slow.val != fast.val) {
+                return false;
+            }
+
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return true;
+
+    }
 }
