16e95554022a64dc6543f5adbae9d2e27b0d0d75
wch.acct@outlook.com <wch.acct@outlook.com>
2022-03-11 09:35:00 +0800

    BM17 二分查找-I

--- src/bm17/Solution.java
+++ src/bm17/Solution.java
@@ -1,5 +1,33 @@
 import java.util.*;
 
+/**
+ * BM17 二分查找-I
+ */
 public class Solution {
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * 
+     * @param nums   int整型一维数组
+     * @param target int整型
+     * @return int整型
+     */
+    public int search(int[] nums, int target) {
 
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
 }
