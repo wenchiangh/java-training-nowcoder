96cbbfe317ea5208100d9cbcf534ed40d80e65bd
wch.acct@outlook.com <wch.acct@outlook.com>
2022-03-11 14:46:42 +0800

    BM20 数组中的逆序对

--- src/bm20/Solution.java
+++ src/bm20/Solution.java
@@ -1,5 +1,20 @@
 import java.util.*;
 
+/**
+ * BM20 数组中的逆序对
+ */
 public class Solution {
+    public int InversePairs(int[] array) {
+        int sortLength = 2;
 
+        while (sortLength < array.length) {
+
+            for (int ii = 0; ii < array.length;) {
+            
+                ii = ii + sortLength;
+            }
+
+            sortLength = sortLength * 2;
+        }
+    }
 }
