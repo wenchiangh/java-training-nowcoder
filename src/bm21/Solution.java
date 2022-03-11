99cc0a6ae6468336ac32282857297eaebe61301d
wch.acct@outlook.com <wch.acct@outlook.com>
2022-03-11 14:59:24 +0800

    BM21 旋转数组的最小数字

--- src/bm21/Solution.java
+++ src/bm21/Solution.java
@@ -1,5 +1,24 @@
 import java.util.*;
 
+/**
+ * BM21 旋转数组的最小数字
+ */
 public class Solution {
+    public int minNumberInRotateArray(int[] array) {
+        int left = 0;
+        int right = array.length - 1;
 
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (array[mid] < array[right]) {
+                right = mid;
+            } else if (array[mid] == array[right]) {
+                right = right - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return array[left];
+    }
 }
